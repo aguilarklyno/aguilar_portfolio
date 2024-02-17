27257df9346081749dcf60172f97f81282ceedeb
aguilarklyno <aguilarklyno@gmail.com>
2024-02-18 03:01:54 +0900

    change route

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,10 +14,10 @@ techs: ["skill1", "skill2", "skill3"],
 link: " url of the project",
 */
   {
-    title: "Portfolio / coming soon...",
-    techs: ["Astro"],
-    link: "https://www.klynoroales-aguilar.com/home",
-    isComingSoon: true,
+    title: "Portfolio / python for finance by Aguilar",
+    techs: ["#document", "#python", "#finance"],
+    link: "https://www.klynoroales-aguilar.com/guides",
+    isComingSoon: false,
   },
 ];
 
